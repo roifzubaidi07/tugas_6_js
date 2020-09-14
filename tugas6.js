eb26ea5c912035c13e3164ae5906f35d74c709b4
M. Ro'if <roifzubaidi5@gmail.com>
2020-09-14 20:55:26 +0700

    Tugas 6 JS
    Tugas ini kami kumpulkan untuk NIOMIC

---
+++ tugas6.js
@@ -0,0 +1,9 @@
+for (let index = 1; index <= 20; index++) {
+  if (index % 3 == 0) {
+    if (index % 2 == 1) {
+      console.log(index);
+    } else {
+      console.log("Bilangan genap");
+    }
+  }
+}
